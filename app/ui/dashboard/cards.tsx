b46ec2c99ce111272314a72f5f5e3d8161f75b9c
sheri <138580481+sheridev1@users.noreply.github.com>
2023-12-29 03:58:17 +0500

    Update cards.tsx

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -17,12 +17,7 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
-  const {
-    numberOfInvoices,
-    numberOfCustomers,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
+  
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}

c9926160be6651a8ec8d659fc67911f401c51eb6
sheridev1 <sheri4439@gmail.com>
2024-01-03 19:14:59 +0500

    loading

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,11 +1,11 @@
 'use server';
+
 export async function createInvoice(formData: FormData) {
-     const rawFormData = {
-       customerId: formData.get('customerId'),
-       amount: formData.get('amount'),
-       status: formData.get('status'),
-     };
-     console.log("woeking")
-     // Test it out:
-     console.log(rawFormData);
+  const rawFormData = {
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  };
+  // Test it out:
+  console.log(rawFormData);
 }
